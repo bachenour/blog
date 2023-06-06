a57b4ae7a76b8fca3b1ee02fda59f8f68471c470
bachenour <bache.nour2@gmail.com>
2023-06-06 23:23:55 +0200

    sending to auteur email

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Auteurs;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -9,16 +10,22 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Mailer\MailerInterface;
 use App\Form\ContactType;
+use Doctrine\Persistence\ManagerRegistry;
 
 
 
 class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'contact')]
+    public function __construct(private readonly ManagerRegistry $doctrine){}
+
+    #[Route('/contact/{id}', name: 'contact')]
     public function contact(Request $request, MailerInterface $mailer)
     {
         $form = $this->createForm(ContactType::class);
         $form->handleRequest($request);
+
+        $entityManager = $this->doctrine->getManager();
+        $auteur = $entityManager->getRepository(Auteurs::class)->find($request->attributes->get('id'));
 
 
         if($form->isSubmitted() && $form->isValid()) {
@@ -27,7 +34,7 @@ class ContactController extends AbstractController
             
             $message = (new Email)
                 ->from($contactFormData['email'])
-                ->to('bache.nour2@gmail.com')
+                ->to($auteur->email)
                 ->subject('Demande de contact : Nouveau message de contact')
                 ->text('Sender : '.$contactFormData['email'].\PHP_EOL.
                     $contactFormData['message'],
